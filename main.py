35dac2739f16b883a7f653dd1bf708b70d60e3d5
CRIS-LI-DEV <131208714+CRIS-LI-DEV@users.noreply.github.com>
2024-03-14 12:48:45 -0300

    123

--- main.py
+++ main.py
@@ -12,13 +12,11 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
-
-
     
 @app.get("/usuarios")
 async def rxxx():
     data_list = [
-    {"nombre": f"juan_{i+1}", "edad": 12 + i, "pais": "chile"} 
-    for i in range(50)
-]
+        {"nombre": f"juan_{i+1}", "edad": 12 + i, "pais": "chile"} 
+        for i in range(50)
+    ]
     return data_list
