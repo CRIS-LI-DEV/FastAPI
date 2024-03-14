a623b7ffcfddcc19fea652fa147618b556c091e2
CRIS-LI-DEV <131208714+CRIS-LI-DEV@users.noreply.github.com>
2024-03-13 22:14:43 -0300

    deuna

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+
+    
+@app.get("/usuarios")
+async def root():
+    data_list = [
+    {"nombre": f"juan_{i+1}", "edad": 12 + i, "pais": "chile"} 
+    for i in range(50)
+]
+    return data_list
